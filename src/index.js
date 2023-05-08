a8db3973f13deefe7eec9df2f112288759d8007d
Andrey Kazubovich <a.kazubovich@itransition.com>
2023-05-08 18:32:08 +0300

    made adaptive and changed to use express

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
-import './styles/style.scss';
-import './styles/header.scss';
-import './styles/content.scss';
-import './styles/footer.scss';
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const PORT = 4200
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+  res.render('index');
+})
+
+app.use('/assets', express.static(path.join(__dirname, '../public')))
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}...`)
+})
